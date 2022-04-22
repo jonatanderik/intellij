21a468583af614b4521bd1da4075d4d7a0921c7e
jonatanderik <jonatanderiks.c@gmial.com>
2022-04-22 13:35:56 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,42 @@ package br.com.dio;
 
 public class PrimeiroPrograma {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Livro livro1 = new Livro("Lorem Ipsum", 300);
+
+        System.out.println(livro1);
+    }
+}
+class Livro {
+   private String nome;
+   private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
